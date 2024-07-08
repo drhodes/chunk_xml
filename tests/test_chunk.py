95cb5bf50aecc78ce3eccec3f7f7440eaa0c1b67
Derek Rhodes <physci@gmail.com>
2024-07-08 11:51:30 -0400

    test cleanup

--- tests/test_chunk.py
+++ tests/test_chunk.py
@@ -5,31 +5,12 @@ from random_tree import random_el
 from chunk_xml.chunk import ChunkMgr
 from chunk_xml.chunk import Chunk
 
-    
+
+def show(el):            
+    return etree.tostring(el, encoding="utf-8").decode("utf-8")
+
+
 class TestChunkMgr():
-    @staticmethod
-    def raw_data1():
-        # ensure the test data
-        return '''
-        <document xmlns="http://cnx.rice.edu/cnxml" xmlns:m="http://www.w3.org/1998/Math/MathML">
-        <title>Solving Systems with Inverses</title>
-        <metadata xmlns:md="http://cnx.rice.edu/mdml">
-        <md:content-id>m49435</md:content-id>
-        <md:title>Solving Systems with Inverses</md:title>
-        <md:abstract><para id="para-00001">In this section, you will:</para><list id="list-00001">
-        <item>Find the inverse of a matrix.</item>
-        <item>Solve a system of linear equations using an inverse matrix.</item>
-        </list></md:abstract>
-        <md:uuid>634d2387-7429-480f-a04e-867c2f9699fb</md:uuid>
-        </metadata>
-        </document>
-        '''
-    
-    # def test_asdf(self):
-    #     root = etree.fromstring(TestChunkMgr.raw_data1())
-    #     cm = ChunkMgr(100, "gpt-4o")
-        
-class TestChunk():
     def hash_cmp(self, xml_string):
         '''
         see if a string can be roundtripped turned into a chunk
@@ -47,18 +28,13 @@ class TestChunk():
     def test_hash_cmp_2(self): self.hash_cmp("<a> <b> ... </b> </a>")
     def test_hash_cmp_3(self): self.hash_cmp("<a> ... <b> ... </b> ... </a>")
     def test_hash_cmp_4(self): self.hash_cmp("<a> <b> asdf </b> asdf </a>")
-    def test_hash_cmp_5(self): self.hash_cmp(TestChunkMgr.raw_data1())
 
     # generate random xml and ensure that hashes match.
     def test_hash_cmd_rand(self):        
-        max_depth = 7
+        max_depth = 5
         for i in range(30):
             s = etree.tostring(random_el(i%max_depth))
             self.hash_cmp(s)
-
-
-def show(el):            
-    return etree.tostring(el, encoding="utf-8").decode("utf-8")
 
 
 def test_decompose_1():
@@ -74,8 +50,6 @@ def test_decompose_1():
     cm = ChunkMgr(100, "gpt-4o")
     chunks = cm.decompose(el)
 
-
-
 def test_decompose_2():
     xml = '''
     <e>
@@ -86,8 +60,10 @@ def test_decompose_2():
     </e>
     '''
     el = etree.fromstring(xml)
-    cm = ChunkMgr(100, "gpt-4o")
+    cm = ChunkMgr(100, "gpt-4o")    
     xs = cm.decompose(el)
+    for x in xs:
+        print(str(xs))
     
 def test_decompose_rand():
     cm = ChunkMgr(500, "gpt-4o")
